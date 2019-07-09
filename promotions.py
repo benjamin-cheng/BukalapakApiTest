60486b326120323688c397be0ad338d1e4f7ccc2
Benjamin Cheng <chengwc@gmail.com>
2019-07-09 11:31:38 +0800

    Code refine

--- promotions.py
+++ promotions.py
@@ -1,18 +1,20 @@
+import sys
 import json
 import requests
 
-url = 'https://www.bukalapak.com/auth_proxies/request_token'
 headers = {'Content-Type': "application/json; charset=utf-8"}
-res = requests.post(url, headers=headers)
-print (res.status_code)
-print (res.raise_for_status())
-print (res.text)
-j = json.loads(res.text)
-print (j['access_token'])
 
-url2 = "https://api.bukalapak.com/info/promotions?sort=-start_date&limit=15&offset=0&access_token=" + j['access_token']
-headers2 = {'Content-Type': "application/json; charset=utf-8"}
-res2 = requests.get(url2, headers=headers2)
-print (res2.status_code)
-print (res2.raise_for_status())
-print (res2.text)
+token_url = 'https://www.bukalapak.com/auth_proxies/request_token'
+token_response = requests.post(token_url, headers=headers)
+print('Get token status code: ' + str(token_response.status_code))
+print('Raise for status: ' + str(token_response.raise_for_status()))
+print('Token result:\n' + token_response.text)
+token_json = json.loads(token_response.text)
+access_token = token_json['access_token']
+
+promotions_url = 'https://api.bukalapak.com/info/promotions?sort=-start_date&limit=15&offset=0&access_token=' + access_token
+promotions_response = requests.get(promotions_url, headers=headers)
+print('\n')
+print('Get promotions status code:' + str(promotions_response.status_code))
+print('Raise for status:' + str(promotions_response.raise_for_status()))
+print('Promotions result:\n' + promotions_response.text)
